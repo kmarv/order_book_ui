5c78fa4010e918a9ae6f57fc7dd0bd3f5eae514d
kmarv <marvkaay@gmail.com>
2023-09-12 11:19:40 +0300

    First commit

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../assets/css/home.scss'
+
+
+function Home() {
+  return (
+    <div className='main__container'>
+        <div className='main__div'></div>
+    </div>
+  )
+}
+
+export default Home;
