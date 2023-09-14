366ba0521f900087014b12428109e3a441085a20
kmarv <marvkaay@gmail.com>
2023-09-14 18:15:57 +0300

    first commit for order book ui

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,114 @@
 import React, { useState } from "react";
+import { DownCircleOutlined } from "@ant-design/icons";
+import { Divider, Select, Input, Slider, Menu, Dropdown } from "antd";
+
 import "../assets/css/home.scss";
-import logo from "../assets/img/logo.png";
-import logo2 from "../assets/img/logo 2.png";
-import { WalletOutlined, LogoutOutlined } from "@ant-design/icons";
-import { Button, Divider, Select, Input } from "antd";
 
 const { Option } = Select;
 
+const marks = {
+  0: "0%",
+  25: "25%",
+  50: "50%",
+  75: "75%",
+  100: {
+    style: {
+      color: "#f50",
+    },
+    label: <strong>100%</strong>,
+  },
+};
+
+function importAll(r) {
+  let images = {};
+  r.keys().forEach((item, index) => {
+    images[item.replace("./", "")] = r(item);
+  });
+  return images;
+}
+
+const menuOptions = [
+  { key: "Ethereum", label: "Ethereum", icon: "eth.png" },
+  { key: "BSC", label: "Binance Smart", icon: "bsc.png" },
+  {key: "SOL", label: "Solano", icon:"sol.png"},
+  {key: 'TRON', label: "TRON", icon:"tron.png"}
+];
+const marketOptions = [
+  {
+    key: "$PIE/USDT", icon:"logo-01.png",
+  },{
+    key: "ETH/USDT", icon:"eth.png",
+  },{
+    key: 'BTC/USDT', icon:"btc.png",
+  },{
+    key:"BNB/USDT", icon:"bsc.png"
+  }
+]
+
 function Home() {
-  function handleChange(value) {
-    console.log(`selected ${value}`);
+  const [activeTab, setActiveTab] = useState(1);
+  const [activeOrderTab, setActiveOrderTab] = useState(1);
+  const [focus, setFocus] = useState(0);
+  const [activeMenuItem, setActiveMenuItem] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(menuOptions[0]);
+  const [marketOption, setMarketOption] = useState("$PIE/USDT");
+  const [tokenImg, setTokenImg] = useState(marketOptions[0].icon)
+
+  // importing all images in the application
+  const images = importAll(
+    require.context("../assets/img", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  // handler for networks 
+  const handleOptionClick = (optionKey) => {
+    const option = menuOptions.find((opt) => opt.key === optionKey);
+    if (option) {
+      setSelectedOption(option);
+    }
+  };
+
+// handler for token images
+  const handleTokenImageChange = (optionKey) =>{
+    const option = marketOptions.find((opt) => opt.key === optionKey);
+    if (option) {
+      setTokenImg(option.icon);
+    }
   }
 
-  const[activeTab, setActiveTab] = useState(1);
-  const[focus, setFocus] = useState(0);
-  const handleTabClick1 =(tabNumber) => {
-    setFocus(tabNumber)
-  }
+  const menu = (
+    <Menu onClick={(e) => handleOptionClick(e.key)}>
+      {menuOptions.map((option) => (
+        <Menu.Item key={option.key} className="network__item">
+          <img
+            src={images[option.icon]}
+            height={30}
+            alt={`${option.key}_logo`}
+          />
+          {option.label}
+        </Menu.Item>
+      ))}
+    </Menu>
+  );
+
+  const handleTabClick1 = (tabNumber) => {
+    setFocus(tabNumber);
+  };
   const handleTabClick = (tabNumber) => {
     setActiveTab(tabNumber);
   };
+  const handleTabClick2 = (tabNumber) => {
+    setActiveOrderTab(tabNumber);
+  };
+
+  function handleChange(value) {
+    setMarketOption(value);
+    handleTokenImageChange(value);
+  }
+  const handleMenuItemClick = (index) => {
+    setActiveMenuItem(index);
+  };
+
+  // console.log(tokenImg);
 
   return (
     <div className="main__container">
@@ -30,26 +119,67 @@ function Home() {
               <div className="nav">
                 <div className="nav__bar">
                   <div className="logo">
-                    <img src={logo} height={80} alt="logo" /> Pie Trade
+                    <img src={images["logo.png"]} height={80} alt="logo" /> Pie
+                    Trade
                   </div>
                   <div className="nav__menu">
-                    <div>Trade</div>
-                    <div>Swap</div>
-                    <div>Earn</div>
-                    <div>Account</div>
-                    <div>More</div>
+                    <div
+                      className={`menu-item ${
+                        activeMenuItem === 0 ? "active" : ""
+                      }`}
+                      onClick={() => handleMenuItemClick(0)}
+                    >
+                      Trade
+                    </div>
+                    <div
+                      className={`menu-item ${
+                        activeMenuItem === 1 ? "active" : ""
+                      }`}
+                      onClick={() => handleMenuItemClick(1)}
+                    >
+                      Swap
+                    </div>
+                    <div
+                      className={`menu-item ${
+                        activeMenuItem === 2 ? "active" : ""
+                      }`}
+                      onClick={() => handleMenuItemClick(2)}
+                    >
+                      Earn
+                    </div>
+                    <div
+                      className={`menu-item ${
+                        activeMenuItem === 3 ? "active" : ""
+                      }`}
+                      onClick={() => handleMenuItemClick(3)}
+                    >
+                      Account
+                    </div>
+                    <div
+                      className={`menu-item ${
+                        activeMenuItem === 4 ? "active" : ""
+                      }`}
+                      onClick={() => handleMenuItemClick(4)}
+                    >
+                      More
+                    </div>
                   </div>
                 </div>
                 <div className="nav__btn">
                   <div className="wallet">
-                    <Button type="primary" icon={<WalletOutlined />}>
-                      wallet
-                    </Button>
+                    <Dropdown overlay={menu} trigger={["click"]}>
+                      <button className="wallet__btn">
+                        <img
+                          src={images[selectedOption.icon]}
+                          height={30}
+                          alt={`${selectedOption.key}_logo`}
+                        />
+                        {selectedOption.label} <DownCircleOutlined />
+                      </button>
+                    </Dropdown>
                   </div>
                   <div className="account">
-                    <Button type="primary" icon={<LogoutOutlined />}>
-                      Log Out
-                    </Button>
+                    <button className="connect__btn">Connect Wallet</button>
                   </div>
                 </div>
               </div>
@@ -59,18 +189,20 @@ function Home() {
             <div className="col-md-12 token">
               <div className="token__info">
                 <div>
-                  <img src={logo2} height={35} alt="" /> $PIE/USDT
+                  <img src={ images[tokenImg]} height={35} alt="" />{" "}
+                  {marketOption}
                 </div>
                 <div>
                   <Select
                     style={{ width: 120 }}
                     onChange={handleChange}
                     placeholder="All Markets"
-                    disabled
+                    bordered={false}
                   >
-                    <Option value="jack">Jack</Option>
-                    <Option value="lucy">Lucy</Option>
-                    <Option value="Yiminghe">yiminghe</Option>
+                    <Option value="BNB/USDT">BNB/USDT</Option>
+                    <Option value="BTC/USDT">BTC/USDT</Option>
+                    <Option value="ETH/USDT">ETH/USDT</Option>
+                    <Option value="$PIE/USDT">$PIE/USDT</Option>
                   </Select>
                 </div>
               </div>
@@ -103,25 +235,141 @@ function Home() {
               </div>
             </div>
           </div>
-          <div className="row" style={{marginTop:"12px"}}>
+          <div className="row" style={{ marginTop: "12px" }}>
             <div className="col-md-3">
               <div className="trade_order">
                 <div className="tabs">
-                  <div  className={`tab-button ${activeTab === 1 ? 'active' : ''}`} onClick={() => handleTabClick(1)}>BUY</div>
-                  <div  className={`tab-button tt ${activeTab === 2 ? 'active' : ''}`} onClick={() => handleTabClick(2)}>Sell</div>
+                  <div
+                    className={`tab-button ${activeTab === 1 ? "active" : ""}`}
+                    onClick={() => handleTabClick(1)}
+                  >
+                    BUY
+                  </div>
+                  <div
+                    className={`tab-button tt ${
+                      activeTab === 2 ? "active" : ""
+                    }`}
+                    onClick={() => handleTabClick(2)}
+                  >
+                    Sell
+                  </div>
                 </div>
                 <div className="tabs__order">
-                  <div className={`order ${focus === 1 ? 'clicked': ''}`} onClick={()=> { handleTabClick1(1)}}>Limit</div>
-                  <div className={`order ${focus === 2 ? 'clicked': ''}`} onClick={()=> { handleTabClick1(2)}}>Market</div>
-                  <div className={`order ${focus === 3 ? 'clicked': ''}`} onClick={()=> { handleTabClick1(3)}}>Stop Limit</div>
+                  <div
+                    className={`order ${focus === 1 ? "clicked" : ""}`}
+                    onClick={() => {
+                      handleTabClick1(1);
+                    }}
+                  >
+                    Limit
+                  </div>
+                  <div
+                    className={`order ${focus === 2 ? "clicked" : ""}`}
+                    onClick={() => {
+                      handleTabClick1(2);
+                    }}
+                  >
+                    Market
+                  </div>
+                  <div
+                    className={`order ${focus === 3 ? "clicked" : ""}`}
+                    onClick={() => {
+                      handleTabClick1(3);
+                    }}
+                  >
+                    Stop Limit
+                  </div>
                 </div>
                 <div className="price__input">
-                  <span>Price</span>  <Input width={100} placeholder="Borderless" bordered={false} suffix="USDT" /> 
+                  <span>Price</span>{" "}
+                  <Input
+                    width={100}
+                    placeholder="20000"
+                    bordered={false}
+                    suffix="USDT"
+                  />
                 </div>
+                <div className="price__input">
+                  <span>Amount</span>{" "}
+                  <Input
+                    width={100}
+                    placeholder="20000"
+                    bordered={false}
+                    suffix="$PIE"
+                  />
+                </div>
+                <div className="price__slider">
+                  <Slider marks={marks} step={10} defaultValue={50} />
+                </div>
+                <div className="price__input">
+                  <span>Total</span>{" "}
+                  <Input
+                    width={100}
+                    placeholder="20000"
+                    bordered={false}
+                    suffix="$PIE"
+                  />
+                </div>
+                <div className="summary">
+                  <div className="summ__title">Transaction Summary</div>
+                  <div className="summ__details">
+                    <span>Entry Price</span> <span>$1000</span>
+                  </div>
+                  <div className="summ__details">
+                    <span>Price Impact</span> <span>10</span>
+                  </div>
+                  <div className="summ__details">
+                    <span>Liquidation Price</span> <span>$1000</span>
+                  </div>
+                  <div className="summ__details">
+                    <span>Transaction fee</span> <span>$1</span>
+                  </div>
+                  <Divider type="horizontal" style={{margin:"10px", padding:"10px"}} />
+                  <div className="summ__details ddd">
+                    <span>Total Cost</span> <span>$20000</span>
+                  </div>
+                </div>
+                <button className="order__button">Place Buy Order</button>
               </div>
             </div>
             <div className="col-md-9">
-              fdgfgfd
+              <div className="row">
+                <div className="col-md-3">
+                  <div className="order__graph">
+                    <div className="graph__tabs">
+                      <div
+                        className={`tab-button-graph ${
+                          activeOrderTab === 1 ? "active" : ""
+                        }`}
+                        onClick={() => handleTabClick2(1)}
+                      >
+                        Book
+                      </div>
+                      <div
+                        className={`tab-button-graph ${
+                          activeOrderTab === 2 ? "active" : ""
+                        }`}
+                        onClick={() => handleTabClick2(2)}
+                      >
+                        Trade
+                      </div>
+                    </div>
+                    <div className="graph__order__tabs">
+                      <div className="graph__title">Total</div>
+                      <div className="graph__title">Amount</div>
+                      <div className="graph__title">Price</div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-9">
+                  <div className="candle__stick"></div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="order_history"></div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
